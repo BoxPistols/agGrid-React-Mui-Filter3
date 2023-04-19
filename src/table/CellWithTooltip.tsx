aeb22b6e74688b987c3726ada349845386ce03f6
A_ito <BoxPistols@users.noreply.github.com>
2023-04-19 18:54:32 +0000

    Update index.js and CellWithTooltip.tsx

--- src/table/CellWithTooltip.tsx
+++ src/table/CellWithTooltip.tsx
@@ -4,11 +4,19 @@ import { Tooltip, Box } from "@mui/material";
 const CellWithTooltip = (props: any) => {
   const { value } = props;
 
+  const defaultTooltipClasses = {
+    tooltip: "no-animation", // アニメーションを無効化するCSSクラスを追加
+  };
+
   return (
     <Tooltip
       title={value}
       //   arrow
-      disableInteractive
+      //   disableInteractive
+      enterDelay={1200}
+      leaveDelay={0}
+      enterTouchDelay={1200}
+      classes={defaultTooltipClasses} // 追加したCSSクラスを適用
       PopperProps={{
         sx: {
           //   minWidth: 100,
@@ -18,8 +26,9 @@ const CellWithTooltip = (props: any) => {
           border: "1px solid #999",
           backgroundColor: "#212121",
           color: "white",
+          transition: "none !important",
           //   marginRight: "32px !important",
-          transition: 0,
+          //   transition: 0,
           //  "& .MuiTooltip-arrow": {
           // top: "-10px !important",
           //    "&::before": {
